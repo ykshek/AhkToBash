56c963f5f7742e87f07ff8e4cdccdb9604e23ffe
Alex Shek <hms.starryfish@gmail.com>
2025-12-11 10:42:33 +0800

    Add headless print

--- src/main/java/userInterface.java
+++ src/main/java/userInterface.java
@@ -16,7 +16,7 @@ public class userInterface extends JFrame
 
    public static void main(String[] args)
    {
-      System.out.println(GraphicsEnvironment.isHeadless());
+      System.out.println("Headless Java: "+GraphicsEnvironment.isHeadless());
       userInterface frame = new userInterface();
       frame.setTitle("Autohotkey to ydotool bash script");
       frame.setVisible(true);
